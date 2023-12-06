78857f740d2a79f48f2634bfc2137acf0f562b72
miafate <miapurplefroot@gmail.com>
2023-12-06 12:25:32 -0300

    init and day01 part1

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -1,5 +1,25 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input: string): number {
-  return 0;
+  const arrOfLines: string[] = input.split('\n')//.map((line) => parseInt(line));
+  arrOfLines.pop();
+  let result: number = 0;
+
+  arrOfLines.forEach(line => {
+    result += filterLineNumber(line)
+  })
+
+  return result;
+}
+
+function filterLineNumber(line: string): number {
+  const arrayOfNumbers = line.split("").filter(x => {
+    if (!isNaN(parseInt(x))) {
+      return x
+    }
+  })
+  const addedNumbers = arrayOfNumbers.length > 1 ? (arrayOfNumbers[0] + arrayOfNumbers[arrayOfNumbers.length - 1]) : (arrayOfNumbers[0] + arrayOfNumbers[0])
+
+
+  return parseInt(addedNumbers);
 }
--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,5 +1,29 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(input: string): number {
-  return 0;
+  const arrOfLines: string[] = input.split('\n')//.map((line) => parseInt(line));
+  arrOfLines.pop();
+  //console.log(arrOfLines);
+  let result: number = 0;
+  arrOfLines.forEach(line => {
+    result += filterLineNumber(line)
+  })
+
+  return result;
+}
+
+const writedNumbers: string[] = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+function filterLineNumber(line: string): number {
+  let lineFull = line
+  writedNumbers.forEach((element, index) => {
+    if (line.includes(element)) {
+      lineFull = line.replaceAll(element, (index + 1).toString())
+      line = lineFull
+    }
+  });
+  console.log(lineFull);
+
+
+  return 0
 }
