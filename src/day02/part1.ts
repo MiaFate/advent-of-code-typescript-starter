9df8f10eb454457d8e56bfea6dde1b73d16494d0
miafate <miapurplefroot@gmail.com>
2023-12-07 19:31:15 -0300

    refactor day02 part1

--- src/day02/part1.ts
+++ src/day02/part1.ts
@@ -20,26 +20,20 @@ export function part1(input: string): number {
 function findIdOfPossibleGame(line: string) {
 
   const lineArray: string[] = line.split(':')
-  let gameId: number = parseInt(lineArray[0].split(" ").at(-1)!);
+  let gameId: number = parseInt(lineArray[0].split(" ")[1]) ?? 0;
   const games: string[] = lineArray[1].split(";")
 
   games.forEach(gameAsString => {
     const bagCubes: Game = { red: 12, green: 13, blue: 14 }
 
-    const gameArray: (string | number)[][] = gameAsString.trim().split(',').map(setAsString => {
+    const gameArray: (string | number)[][] = gameAsString?.trim().split(',').map(setAsString => {
       const [qty, colour] = setAsString.trim().split(" ") ?? ['0', 'red']
       return [colour, parseInt(qty)]
-    }) ?? [['blue', 0], ['green', 0], ['red', 0]]
+    })
 
     const game: Game = Object.fromEntries(gameArray);
 
-    if (game.red > bagCubes.red) {
-      gameId = 0
-    }
-    if (game.green > bagCubes.green) {
-      gameId = 0
-    }
-    if (game.blue > bagCubes.blue) {
+    if (game.red > bagCubes.red || game.green > bagCubes.green || game.blue > bagCubes.blue) {
       gameId = 0
     }
 
