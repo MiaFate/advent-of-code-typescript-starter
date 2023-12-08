8da74b5453e2d80448fc0ddbe05e60d69eebfea7
miafate <miapurplefroot@gmail.com>
2023-12-07 21:44:58 -0300

    start day02 part2

--- src/day02/part1.ts
+++ src/day02/part1.ts
@@ -4,6 +4,7 @@ interface Game {
   green: number;
   blue: number;
 }
+const bagCubes = { red: 12, green: 13, blue: 14 } as const
 
 export function part1(input: string): number {
   if (input.trim() == "") return 0
@@ -20,20 +21,19 @@ export function part1(input: string): number {
 function findIdOfPossibleGame(line: string) {
 
   const lineArray: string[] = line.split(':')
-  let gameId: number = parseInt(lineArray[0].split(" ")[1]) ?? 0;
+  let gameId: number = parseInt(lineArray[0].split(" ").at(-1) ?? '0');
   const games: string[] = lineArray[1].split(";")
 
   games.forEach(gameAsString => {
-    const bagCubes: Game = { red: 12, green: 13, blue: 14 }
 
     const gameArray: (string | number)[][] = gameAsString?.trim().split(',').map(setAsString => {
       const [qty, colour] = setAsString.trim().split(" ") ?? ['0', 'red']
       return [colour, parseInt(qty)]
     })
 
-    const game: Game = Object.fromEntries(gameArray);
-
-    if (game.red > bagCubes.red || game.green > bagCubes.green || game.blue > bagCubes.blue) {
+    const gameObject: Game = Object.fromEntries(gameArray);
+    //      ^?
+    if (gameObject.red > bagCubes.red || gameObject.green > bagCubes.green || gameObject.blue > bagCubes.blue) {
       gameId = 0
     }
 
--- src/day02/part2.ts
+++ src/day02/part2.ts
@@ -1,5 +1,43 @@
 // Advent of Code - Day 2 - Part Two
-
+// Advent of Code - Day 2 - Part One
+interface Game {
+  red: number;
+  green: number;
+  blue: number;
+}
+const bagCubes = { red: 12, green: 13, blue: 14 } as const
 export function part2(input: string): number {
-  return 0;
+  const arrayOfLines: string[] = input.split('\n');
+  if (input.trim() == "") return 0
+  let result = 0;
+
+  arrayOfLines.forEach(line => {
+    result += findIdOfPossibleGame(line)
+  })
+
+  return result;
+}
+
+function findIdOfPossibleGame(line: string) {
+
+  const lineArray: string[] = line.split(':')
+  let gameId: number = parseInt(lineArray[0].split(" ").at(-1) ?? '0');
+  const games: string[] = lineArray[1].split(";")
+
+  games.forEach(gameAsString => {
+
+    const gameArray: (string | number)[][] = gameAsString?.trim().split(',').map(setAsString => {
+      const [qty, colour] = setAsString.trim().split(" ") ?? ['0', 'red']
+      return [colour, parseInt(qty)]
+    })
+
+    const gameObject: Game = Object.fromEntries(gameArray);
+    //      ^?
+    if (gameObject.red > bagCubes.red || gameObject.green > bagCubes.green || gameObject.blue > bagCubes.blue) {
+      gameId = 0
+    }
+
+  });
+
+  return gameId
 }
